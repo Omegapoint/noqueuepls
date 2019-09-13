aa88a2432674dcab47c85650cb664aac673bde45
simvel <simon.velander@omegapoint.se>
2019-09-13 17:33:41 +0200

    Made screenshotter return base64 for real

--- queuer/screenshoter.py
+++ queuer/screenshoter.py
@@ -1,11 +1,17 @@
-from subprocess import call
+import pyautogui
 import os
+import base64
 
 def take_screenshot():
     name = "wowqueue.jpg"
-    call(["screencapture", name])
+    pyautogui.screenshot(name)
+    with open("./wowqueue.jpg", "rb") as f:
+        data = f.read()
+        print (base64.b64encode(data))
     return name
 
 def delete_screenshot(name):
     os.remove(name)
     print("removed screenshot")
+
+take_screenshot()
