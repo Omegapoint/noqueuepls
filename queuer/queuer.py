663cff3590f3d3d3dea21c1e7cd14e00c50d3577
simvel <simon.velander@omegapoint.se>
2019-09-13 16:51:39 +0200

    Clean up of queuer

--- queuer/queuer.py
+++ queuer/queuer.py
@@ -3,37 +3,25 @@ import requests
 import re
 import json
 
-def get_queue(file_path):
+def get_queue(imageData):
     print("checking queue...")
-    jsonRes = json.loads(ocr_space_file(file_path))
+    jsonRes = json.loads(ocr_space_file(imageData))
     parsedText = jsonRes['ParsedResults'][0]['ParsedText']
     queue = re.search("\d+", parsedText).group(0)
     print("queue is:")
     print(queue)
     return queue
 
-def ocr_space_file(filename, overlay=True, api_key='9c1888603288957', language='eng'):
+image = "hej";
+
+def ocr_space_file(overlay=True, api_key='9c1888603288957', language='eng'):
     payload = {'isOverlayRequired': overlay,
                'apikey': api_key,
                'language': language,
+               'base64Image': 'data:image/png;base64,' + image,
                }
-    with open(filename, 'rb') as f:
-        r = requests.post('https://api.ocr.space/parse/image',
-                          files={filename: f},
-                          data=payload,
-                          )
-    return r.content.decode()
 
-
-def ocr_space_url(url, overlay=True, api_key='9c1888603288957', language='eng'):
-    payload = {'url': url,
-               'isOverlayRequired': overlay,
-               'apikey': api_key,
-               'language': language,
-               }
-    r = requests.post('https://api.ocr.space/parse/image',
-                      data=payload,
-                      )
+    r = requests.post('https://api.ocr.space/parse/image', data=payload)
     return r.content.decode()
 
 get_queue('/Users/gabrielvilen/Downloads/queue.jpg')
